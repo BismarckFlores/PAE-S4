4fd0ce439c11aa8b53f3e759a70442da2cca072b
Bismarck Flores <bjflores@uamv.edu.ni>
2026-09-08 16:04:28 -0600

    feature: Agredar clases Category y Product

--- Facturation/src/main/java/com/uam/facturationapp/model/Category.java
+++ Facturation/src/main/java/com/uam/facturationapp/model/Category.java
@@ -1,4 +1,17 @@
 package com.uam.facturationapp.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    private Integer id;
+    private String name;
+    private boolean active;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- Facturation/src/main/java/com/uam/facturationapp/model/Product.java
+++ Facturation/src/main/java/com/uam/facturationapp/model/Product.java
@@ -1,4 +1,21 @@
 package com.uam.facturationapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private Integer id;
+    private String code;
+    private String name;
+    private Category category;
+    private BigDecimal salePrice;
+    private int stock;
+    private String imagePath;
+    private boolean active;
 }
