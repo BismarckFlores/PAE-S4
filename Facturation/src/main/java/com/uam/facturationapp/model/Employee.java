41cf7cd88dab858e2a01b42ac3be292bcee2a58f
Bismarck Flores <bjflores@uamv.edu.ni>
2026-09-08 16:22:11 -0600

    Update: Clases Employee y Position

--- Facturation/src/main/java/com/uam/facturationapp/model/Employee.java
+++ Facturation/src/main/java/com/uam/facturationapp/model/Employee.java
@@ -1,4 +1,19 @@
 package com.uam.facturationapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+    private Integer id;
+    private String names;
+    private String lastname;
+    private Position position;
+    private LocalDate hireDate;
+    private boolean active;
 }
--- Facturation/src/main/java/com/uam/facturationapp/model/Position.java
+++ Facturation/src/main/java/com/uam/facturationapp/model/Position.java
@@ -1,4 +1,14 @@
 package com.uam.facturationapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Position {
+    private Integer id;
+    private String name;
+    private String desc;
 }
